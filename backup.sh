2a9f559c8a464c8278a2b71e7798a8c1cc108efa
groegercesg <groegercesg@users.noreply.github.com>
2022-01-06 16:34:40 +0000

    RPM Packages

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@
 set -e
 
 date=$(date '+%H:%M:%S-%m-%d-%Y')
-backup=fedora-installed-packages-$date
+backup=package-backup-$date
 
 echo "Linux Package Backup program"
 
@@ -16,40 +16,60 @@ fi
 
 # Sub-functions for backup
 snaps() {
+    cd $backup
     input=$(snap list | cut -f -1 -d ' ')
     echo "$input" | sed 1d > snaps.list
+    cd ..
     echo "Snaps list saved"
 }
 
 apt_packages() {
+    cd $backup
     sudo apt list --manual-installed | grep "\[installed\]" | cut -f -1 -d '/' > apt_package.list
+    cd ..
     echo "Apt Packages list saved"
 }
 
 flatpaks() {
+    cd $backup
     # For this, we are simply saving names of packages, on restore Flatpak will then go and install the most recent version.
     flatpak list | cut -f -1 | uniq -u > flatpak.list
+    cd ..
     echo "Flatpak list saved"
 }
 
 dnf_packages() {
+    cd $backup
     dnf list installed | grep -v "Installed" | tr -s \  \\t | cut -f 1 > dnf_packages.list
+    cd ..
     echo "Dnf Packages list saved"
 }
 
 yum_packages() {
+    cd $backup
     yum list installed | grep -v "Installed" | tr -s \  \\t | cut -f 1 > yum_packages.list
+    cd ..
     echo "Yum Packages list saved"
+}
+
+rpm_packages() {
+    cd $backup
+    rpm -qa > rpm_packages.list
+    cd ..
+    echo "RPM Packages list saved"
 }
 
 # Functions for backup and restore
 backup() {
     echo "Backup Mode"
+    rm -rf $backup
+    mkdir $backup
     snaps
     apt_packages
     flatpaks
     dnf_packages
     yum_packages
+    rpm_packages
 }
 
 restore() {
